69316836ead3a83a86222910219ec8c2533442d6
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-05-18 13:39:56 -0500

    Add author show page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,8 @@ class AuthorsController < ApplicationController
   def index
     @authors = Author.order(:name).page(params[:page])
   end
+
+  def show
+    @author = Author.find(params[:id])
+  end
 end
