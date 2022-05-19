9b82628f99043d48e4fba7e6491a2e44c4eadcfb
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-05-19 08:41:44 -0500

    Refactoring data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do
+50.times do
   FactoryBot.create(:author)
 end
 
@@ -14,6 +14,6 @@ end
   Category.create name: "Category #{number}"
 end
 
-200.times do
+2_000.times do
   FactoryBot.create(:book)
 end
--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :book do
     code { Faker::Internet.uuid }
-    name { Faker::Lorem.word }
+    name { Faker::Name.first_name }
     description { Faker::Lorem.sentence }
     price { Faker::Number.decimal(l_digits: 4, r_digits: 2) }
     number_of_pages { Faker::Number.number(digits: 3) }
     date_published { Faker::Date.in_date_period }
     active { [true, false].sample }
-    author_id { rand(1..100) }
+    author_id { rand(1..50) }
     category_id { rand(1..10) }
   end
 end
