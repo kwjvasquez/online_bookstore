efdf4c4a320934abdbed16ef9c12cdb2f4dfce20
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-05-20 17:16:38 -0500

    Add Comment model test

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Comment, type: :model do
+  subject { create(:comment, book_id: 1, user_id: 1) }
+
+  let!(:user) { create(:user) }
+  let!(:author) { create(:author) }
+  let!(:category) { create(:category) }
+  let!(:book) { create(:book, author_id: 1, category_id: 1, active: false) }
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:message) }
+    it { is_expected.to validate_uniqueness_of(:book_id).scoped_to(:user_id) }
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:book) }
+    it { is_expected.to belong_to(:user) }
+  end
+end
