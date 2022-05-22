9e69ff75981476ceb308a30d7407d18c21bb6893
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-05-22 17:55:38 -0500

    Add tag show page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,10 @@ class TagsController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tag.order(:name).page(params[:page])
   end
+
+  def show
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+    @books_related = Book.tagged_with(@tag.name).order(:name).page(params[:page])
+    @books_with_this_tag = @books_related.count
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tags_names = ["tag 1", "tag 2", "tag 3"]
 
 5.times do
   FactoryBot.create(:author)
@@ -16,8 +10,7 @@ end
 
 10.times do
   book = FactoryBot.create(:book)
-  word_list = Faker::Lorem.words.join(", ")
-  book.tag_list.add(word_list, parse: true)
+  book.tag_list.add(tags_names.sample)
   book.save
 end
 
