7ea5795d66affa5b02c6372add2a177971239281
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-05-22 09:00:11 -0500

    Add associations to Author model test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,8 +5,12 @@ require "rails_helper"
 RSpec.describe Author, type: :model do
   subject { create(:author) }
 
+  describe "associations" do
+    it { is_expected.to have_many(:books).dependent(:destroy) }
+  end
+
   describe "validations" do
-    it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 end
