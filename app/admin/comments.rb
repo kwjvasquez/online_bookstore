a2ad8feb0fac7ec216f616af85a9daf65d20a65b
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-05-31 11:40:44 -0500

    Add change to comments forms

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -3,6 +3,28 @@
 ActiveAdmin.register Comment do
   permit_params :message, :user_id, :book_id, :approved
 
+  index do
+    selectable_column
+    id_column
+    column :message
+    column :book
+    column :user
+    actions
+  end
+
+  # show email instead of the object reference
+  filter :user, as: :select, collection: User.pluck(:email, :id)
+  filter :book
+
+  form do |f|
+    f.inputs do
+      f.input :user, as: :select, collection: User.pluck(:email, :id)
+      f.input :book
+      f.input :message
+    end
+    f.actions
+  end
+
   action_item :approved, only: :show do
     unless comment.approved
       link_to "Approve", approved_admin_comment_path(comment),
