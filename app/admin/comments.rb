b4f1bf286efa68d393e3131614441b4f1b911a0f
Wilman Junior Vasquez Valdez <97067572+kwjvasquez@users.noreply.github.com>
2022-05-31 16:48:12 -0500

    Feature/add option to change a comment state (#6)
    * Add option to approve comments
    
    * Add option to disapprove comments
    
    * Add change to comments forms
    
    * Address to feedback

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -2,4 +2,52 @@
 
 ActiveAdmin.register Comment do
   permit_params :message, :user_id, :book_id, :approved
+
+  index do
+    selectable_column
+    id_column
+    column :message
+    column :book
+    column :user
+    actions
+  end
+
+  # show email instead of the object reference
+  filter :user, as: :select, collection: User.pluck(:email, :id)
+  filter :book
+
+  form do |f|
+    f.inputs do
+      f.input :user, as: :select, collection: User.pluck(:email, :id)
+      f.input :book
+      f.input :message
+    end
+    f.actions
+  end
+
+  action_item :approved, only: :show do
+    unless comment.approved?
+      link_to "Approve", approved_admin_comment_path(comment),
+              method: :patch
+    end
+  end
+
+  action_item :unapproved, only: :show do
+    if comment.approved?
+      link_to "Unapprove", unapproved_admin_comment_path(comment),
+              method: :patch
+    end
+  end
+
+  member_action :approved, method: :patch do
+    comment = Comment.find(params[:id])
+    comment.update(approved: true)
+    redirect_to admin_comment_path(comment)
+  end
+
+  member_action :unapproved, method: :patch do
+    comment = Comment.find(params[:id])
+    comment.update(approved: false)
+    redirect_to admin_comment_path(comment)
+  end
 end
