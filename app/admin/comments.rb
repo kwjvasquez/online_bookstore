246f2c10a9153bd718a42c555f484a0b313c72fb
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-05-31 11:35:41 -0500

    Add option to approve comments

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -2,4 +2,17 @@
 
 ActiveAdmin.register Comment do
   permit_params :message, :user_id, :book_id, :approved
+
+  action_item :approved, only: :show do
+    unless comment.approved
+      link_to "Approve", approved_admin_comment_path(comment),
+              method: :patch
+    end
+  end
+
+  member_action :approved, method: :patch do
+    comment = Comment.find(params[:id])
+    comment.update(approved: true)
+    redirect_to admin_comment_path(comment)
+  end
 end
