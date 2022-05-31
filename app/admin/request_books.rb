94a7ea167bd4dfc52cd734da0ace33f8b882fc36
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-05-31 17:38:15 -0500

    Add refactoring to Request Book admin views

--- app/admin/request_books.rb
+++ app/admin/request_books.rb
@@ -2,4 +2,26 @@
 
 ActiveAdmin.register RequestBook do
   permit_params :user_id, :book_id
+
+  index do
+    selectable_column
+    id_column
+    column :book
+    column :user
+    column :created_at
+    column :state
+    actions
+  end
+
+  filter :user, as: :select, collection: User.pluck(:email, :id)
+  filter :book
+  filter :state, as: :select, collection: RequestBook.states
+
+  form do |f|
+    f.inputs do
+      f.input :user, as: :select, collection: User.pluck(:email, :id)
+      f.input :book
+    end
+    f.actions
+  end
 end
