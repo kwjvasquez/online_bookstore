d5dfb24fae667f3b4cbf51c1951895f855d627eb
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-06-09 13:57:20 -0500

    Add refactorize to import books

--- app/services/books/import.rb
+++ app/services/books/import.rb
@@ -8,17 +8,12 @@ module Books
 
     def call
       @books.each do |book_data|
-        book = Book.create(
-          code: book_data[:code],
-          name: book_data[:name],
-          description: book_data[:description],
-          price: book_data[:price],
-          number_of_pages: book_data[:number_of_pages],
-          date_published: book_data[:date_published],
-          author_id: book_data[:author_id],
-          category_id: book_data[:category_id]
-        )
-
+        book = Book.find_by(code: book_data[:code])
+        if book
+          book.update(book_data.except(:tags))
+        else
+          book = Book.create(book_data.except(:tags))
+        end
         Books::AddTags.new(book: book, tags: book_data[:tags]).call
       end
     end
