abb1b36465fe08158bac5d4d22860aa3b6d43d91
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-06-13 17:29:40 -0500

    Address to feedback

--- app/services/books/import.rb
+++ app/services/books/import.rb
@@ -8,11 +8,13 @@ module Books
 
     def call
       @books.each do |book_data|
-        book = Book.find_by(code: book_data[:code])
-        if book
-          book.update(book_data.except(:tags))
+        book = Book.find_or_initialize_by(code: book_data[:code]) do |new_book|
+          new_book.assign_attributes(book_data.except(:code, :tags))
+        end
+        if book.new_record?
+          book.save
         else
-          book = Book.create(book_data.except(:tags))
+          book.update(book_data.except(:tags))
         end
         Books::AddTags.new(book: book, tags: book_data[:tags]).call
       end
