7fd9281b6fae257bff4c2e4e14d645ede6d777c9
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-06-21 17:35:34 -0500

    Add omniauth methods and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,22 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :comments, dependent: :destroy
   has_many :request_books
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data["email"]).first
+    user ||= User.create(
+      first_name: data["first_name"],
+      last_name: data["last_name"],
+      email: data["email"],
+      password: Devise.friendly_token[0, 20]
+    )
+
+    user
+  end
 end
