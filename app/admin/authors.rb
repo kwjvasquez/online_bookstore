d9ecbdb414fe9f32e8618a1d45cb97957dd9f1a8
Wilman Vasquez <wilman.vasquez@koombea.com>
2022-06-23 09:30:54 -0500

    Add photo to Author

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Author do
-  permit_params :name
+  permit_params :name, :photo
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :photo, as: :file
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    actions
+  end
+
+  filter :name
+
+  show do
+    attributes_table do
+      row :name
+      row :photo do
+        image_tag resource.photo if resource.photo.attached?
+      end
+    end
+  end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,7 @@
 
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
 end
